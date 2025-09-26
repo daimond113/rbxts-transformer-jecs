f65dd36ff2f123b40a42956a04e643c4d0163548
christopher-buss <christopher.buss+github@protonmail.com>
2025-09-26 14:29:22 +0100

    style(lint): use singular tsconfig for linting

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,19 +1,20 @@
 import eslint from "@eslint/js"
+import { defineConfig, globalIgnores } from "eslint/config"
 import ts from "typescript-eslint"
 import prettier from "eslint-plugin-prettier/recommended"
 
-export default ts.config(eslint.configs.recommended, ts.configs.strict, prettier as never, {
-	ignores: ["out"],
-
-	languageOptions: {
-		parser: ts.parser,
-		ecmaVersion: 2018,
-		sourceType: "module",
-
-		parserOptions: {
-			jsx: true,
-			useJSXTextNode: true,
-			project: "./tsconfig.build.json",
+export default defineConfig(
+	eslint.configs.recommended,
+	ts.configs.strict,
+	{
+		languageOptions: {
+			parserOptions: {
+				projectService: {
+					allowDefaultProject: ["*.ts"],
+				},
+			},
 		},
 	},
-})
+	globalIgnores(["**/out", "test"]),
+	prettier,
+)
